98436040d186cef069d9ce7a006c8670c58618f2
ADebor <adebor@uliege.be>
2023-05-04 16:43:32 +0200

    added empty file

---
+++ omniisaacgymenvs/tasks/sh_effort_finger_task.py
@@ -0,0 +1,4 @@
+"""
+1-finger task to be determined
+probably a task involving pressing a button whose internal resistance/stifness is dynamically changed across learning episodes, until a given threshold or sth
+"""
