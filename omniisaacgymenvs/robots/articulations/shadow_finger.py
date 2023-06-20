d1ee51c19108e80988664828a764d26787720daf
ADebor <adebor@uliege.be>
2023-06-20 04:42:08 -0400

    added finger articulation and view (not tested)

---
+++ omniisaacgymenvs/robots/articulations/shadow_finger.py
@@ -0,0 +1,73 @@
+from typing import Optional
+import numpy as np
+import torch
+from omni.isaac.core.robots.robot import Robot
+from omni.isaac.core.utils.stage import add_reference_to_stage
+from omniisaacgymenvs.tasks.utils.usd_utils import set_drive
+
+from pxr import PhysxSchema
+import os
+
+class ShadowFinger(Robot):
+    def __init__(
+        self,
+        prim_path: str,
+        name: Optional[str] = "shadow_finger",
+        translation: Optional[torch.tensor] = None,
+        orientation: Optional[torch.tensor] = None,
+    ) -> None:
+            
+        self._name = name
+
+        assets_root_path = os.path.dirname(os.path.realpath(__file__)) + "/../.." + "/assets"
+        self._usd_path = (
+            assets_root_path
+            + "/Robots/shadow_finger_instanceable.usd"
+        )
+        
+        self._position = (
+            torch.tensor([0.0, 0.0, 0.5]) if translation is None else translation
+        )
+        self._orientation = (
+            torch.tensor([1.0, 0.0, 0.0, 0.0]) if orientation is None else orientation
+        )
+        add_reference_to_stage(self._usd_path, prim_path)
+        super().__init__(
+            prim_path=prim_path,
+            name=name,
+            translation=self._position,
+            orientation=self._orientation,
+            articulation_controller=None,
+        )
+
+    def set_shadow_finger_properties(self, stage, shadow_finger_prim):
+        for link_prim in shadow_finger_prim.GetChildren():
+            if link_prim.HasAPI(PhysxSchema.PhysxRigidBodyAPI):
+                rb = PhysxSchema.PhysxRigidBodyAPI.Get(stage, link_prim.GetPrimPath())
+                rb.GetDisableGravityAttr().Set(True)
+                rb.GetRetainAccelerationsAttr().Set(True)
+
+    def set_motor_control_mode(self, stage, shadow_hand_path):
+        joints_config = {
+            "robot0_FFJ3": {"stiffness": 0., "damping": 0., "max_force": 0.9},
+            "robot0_FFJ2": {"stiffness": 0., "damping": 0., "max_force": 0.9},
+            "robot0_FFJ1": {"stiffness": 0., "damping": 0., "max_force": 0.7245},
+        }
+
+        # position control config:
+        # joints_config = {
+        #     "robot0_FFJ3": {"stiffness": 1, "damping": 0.1, "max_force": 0.9},
+        #     "robot0_FFJ2": {"stiffness": 1, "damping": 0.1, "max_force": 0.9},
+        #     "robot0_FFJ1": {"stiffness": 1, "damping": 0.1, "max_force": 0.7245},
+        #                 }
+
+        for joint_name, config in joints_config.items():
+            set_drive(
+                f"{self.prim_path}/joints/{joint_name}",
+                "angular",
+                "position",
+                0.0,
+                config["stiffness"] * np.pi / 180,
+                config["damping"] * np.pi / 180,
+                config["max_force"],
+            )
---
+++ omniisaacgymenvs/robots/articulations/views/shadow_finger_view.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+from omni.isaac.core.articulations import ArticulationView
+from omni.isaac.core.prims import RigidPrimView
+
+import torch
+
+
+class ShadowFingerView(ArticulationView):
+    def __init__(
+        self,
+        prim_paths_expr: str,
+        name: Optional[str] = "ShadowFingerView",
+        track_contact_forces=True,
+        prepare_contact_sensors=True,
+    ) -> None:
+        super().__init__(
+            prim_paths_expr=prim_paths_expr, name=name, reset_xform_properties=False
+        )
+
+        # define fingertip view to track contact forces
+        self._tips = RigidPrimView(
+            prim_paths_expr="/World/envs/.*/shadow_finger/robot0.*distal",
+            name="finger_view",
+            reset_xform_properties=False,
+            track_contact_forces=track_contact_forces,
+            prepare_contact_sensors=prepare_contact_sensors,
+        )
+       
+    @property
+    def actuated_dof_indices(self):
+        return self._actuated_dof_indices
+
+    def initialize(self, physics_sim_view):
+        super().initialize(physics_sim_view)
+        self.actuated_joint_names = [
+            "robot0_FFJ3",
+            "robot0_FFJ2",
+            "robot0_FFJ1",
+        ]
+        self._actuated_dof_indices = list()
+        for joint_name in self.actuated_joint_names:
+            self._actuated_dof_indices.append(self.get_dof_index(joint_name))
+        self._actuated_dof_indices.sort()
+
+        print("number of fixed tendons: ", self.num_fixed_tendons)
+
+        limit_stiffness = torch.tensor(
+            [30.0] * self.num_fixed_tendons, device=self._device
+        )
+        damping = torch.tensor([0.1] * self.num_fixed_tendons, device=self._device)
+        self.set_fixed_tendon_properties(
+            dampings=damping, limit_stiffnesses=limit_stiffness
+        )
